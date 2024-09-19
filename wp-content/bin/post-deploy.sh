b86bb31ff6260516c6dd7e19a7eeed92ac7748a5
Ollie-D7 <ollie.clark@domain7.com>
2024-09-19 17:37:25 +0100

    Fix post-deploy file

--- wp-content/bin/post-deploy.sh
+++ wp-content/bin/post-deploy.sh
@@ -2,6 +2,6 @@
 
 echo "Starting post deploy script..."
 echo "Switch directory to wp-content/cudos"
-cd wp-content/cudos
+cd wp-content/themes/cudos
 echo "Installing Composer dependencies..."
 composer install --no-dev --no-progress
